f346dba49f29c0707fc896d17fccf1efbe98ce4c
Grzegorz Norberciak <grzegorz.norberciak@gmail.com>
2020-04-15 13:22:32 +0200

    maze generator added

--- engine.py
+++ engine.py
@@ -11,17 +11,18 @@ def create_board(BOARD_WIDTH, BOARD_HEIGHT):
     Returns:
     list: Game board
     '''
-    board_material = ((BOARD_WIDTH)//3)*['p']+(((BOARD_WIDTH)//3)*2)*['p']
-    # board_material.remove('w')
-    # board_material.remove('p')
+    board_material = ((BOARD_WIDTH)//3)*['w']+(((BOARD_WIDTH)//3)*2)*['p']
+    board_material.remove('w')
+    board_material.remove('p')
     line = []
     board = []
     line_with_border = []
 
     passages = []
+    no_escape_cell =[]
 
 
-    while len(passages) < BOARD_HEIGHT * 10:
+    while not len(passages) >= BOARD_HEIGHT * 10:
         for row in range(BOARD_HEIGHT):
             for cell in range(BOARD_WIDTH):
                 if row == 0 or row == BOARD_HEIGHT-1:
@@ -36,7 +37,9 @@ def create_board(BOARD_WIDTH, BOARD_HEIGHT):
             for cell in range(1,BOARD_WIDTH-1):
                 if board[row][cell] == 'p' and board[-row][cell] == 'p':
                     passages.append(1)
-
+                if board[row][cell] == 'p' and board[row][-cell] == 'w' and board[row][+cell] == 'w'\
+                    and board[-row][cell] == 'w' and board[+row][cell] == 'w':
+                    continue
     return board
 
     # board = [list(30*'w'),\
