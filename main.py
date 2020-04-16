9f844e7ca348227dba19739fa30ad265a7389fa5
CecyliaM <cecylia.meredith@gmail.com>
2020-04-16 14:03:27 +0200

    update

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
 import util
 import engine
 import ui
+import time
 import os
 '''--------------------Piotrek-----------------------------------------------------------------'''
 import random 
-inventory = {"Health": 100, "Power": 25, "Weapon": 0, "Keys": 0}
+inventory = {"Health": 15, "Weapon": 0, "Eggs": 0}
 '''
 $ is a key to unlock door
 * is a weapon
 ! is health upgrade by 25%
 '''
-items = {"$":1, "*":1, "!":25}
+#items = {"$":1, "*":1, "!":25}
 
 '''--------------------Piotrek-----------------------------------------------------------------'''
-
+'''
+story
+< When life gives you eggs, make an omelette > 
+Welcome, Eggbert!
+You have been teleported to this creepy dungeon.
+Your task is to collect enough eggs to make an omelette.
+Be careful! You are going to encounter chickens that will try to stop you.
+'''
 
 PLAYER_ICON = '@'
 PLAYER_START_X = 3
@@ -44,23 +52,23 @@ def main():
     player = create_player()
     board = engine.create_board(BOARD_WIDTH, BOARD_HEIGHT)
 
-    for i in range(3):
+    for i in range(8):
         item_place = list(random_position(board, BOARD_WIDTH, BOARD_HEIGHT))
         item_x = item_place[0]
         item_y = item_place[1]
         board[item_x][item_y] = "$"
-    for i in range(4):
+    for i in range(2):
         weapon_place = list(random_position(board, BOARD_WIDTH, BOARD_HEIGHT))
         weapon_x = weapon_place[0]
         weapon_y = weapon_place[1]
         board[weapon_x][weapon_y] = "*"
-    for i in range(4):
+    for i in range(5):
         health_place = list(random_position(board, BOARD_WIDTH, BOARD_HEIGHT))
         health_x = health_place[0]
         health_y = health_place[1]
         board[health_x][health_y] = "!"
     
-    for i in range(3):
+    for i in range(4):
         monster_place = list(random_position(board, BOARD_WIDTH, BOARD_HEIGHT))
         monster_x = monster_place[0]
         monster_y = monster_place[1]
@@ -79,6 +87,10 @@ def main():
         key = util.key_pressed()
         if key == 'q':
             is_running = False
+        if inventory['Eggs'] == 5:
+            print("🍳 You have collected enough eggs to make an omelette! 🍳")
+            print("You won!")
+            break
         if key == 'i':
             display_inventory(inventory)
             os.system("""bash -c 'read -s -n 1 -p "Press any key to continue..."'""")
@@ -98,9 +110,27 @@ def main():
             if not board[player_start_X][player_start_Y+1] == "w":
                 board[player_start_X][player_start_Y] = "p"
                 player_start_Y +=1
-        if board[monster_x+1][monster_y] == "p":
-            board[monster_x+1][monster_y] = "m"
-            board[monster_x][monster_y] = "p"
+        if board[player_start_X][player_start_Y] == "!":
+            inventory['Health'] += 1
+        if board[player_start_X][player_start_Y] == "*":
+            inventory['Weapon'] += 1
+        if board[player_start_X][player_start_Y] == "$":
+            inventory['Eggs'] += 1
+     #   if board[player_start_X][player_start_Y] == "m":
+      #      print("The chicken attacks you! You lose 1 health point")
+       #     inventory['Health'] -=1
+        #    print("Press o to attack")
+         #   time.sleep(2)
+          #  print("You defeated the chicken!")
+           # time.sleep(2)
+            #else:
+             #   print("You were defeated!")
+              #  inventory['Health'] -=3
+               # time.sleep(2)
+        if inventory['Health'] == 0:
+            print(inventory['Health'])
+            print("You die!")
+            break
         
         # if key == 'I' or 'i': #Piotrek
         #     display_inventory(inventory, board)
--- ui.py
+++ ui.py
@@ -9,10 +9,12 @@ def display_board(board):
     '''
 
     #board = engine.create_board(30,20)
-    color_scheme = {'p': '  ', 'w': '▓▓', 'g': '[|]', 's': '🧙', '$': ' $', '!': ' !', '*': ' *', 'm': '##'}
+    color_scheme = {'p': '  ', 'w': '⬛', 'g': '[|]', 's': '👲', '$': '🥚', '!': '💟', '*': '🔪', 'm': '🐥'}
     
     for row in board:
         for cell in row: 
             print(color_scheme[cell], end='')
         print()
     print()
+
+#🍗🔥⬜⬛🔲🗡💁‍♂️🧙👲
