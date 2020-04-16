3ed1c9d7ea3110ce2c2970033152395e0c3893c7
Grzegorz Norberciak <grzegorz.norberciak@gmail.com>
2020-04-16 10:03:07 +0200

    better board generator

--- engine.py
+++ engine.py
@@ -14,15 +14,16 @@ def create_board(BOARD_WIDTH, BOARD_HEIGHT):
     board_material = ((BOARD_WIDTH)//3)*['w']+(((BOARD_WIDTH)//3)*2)*['p']
     board_material.remove('w')
     board_material.remove('p')
-    line = []
-    board = []
-    line_with_border = []
 
     passages = []
     no_escape_cell =[]
 
-
-    while not len(passages) >= BOARD_HEIGHT * 10:
+    while not len(passages) >= BOARD_HEIGHT * 10 or len(no_escape_cell) != 0:
+        passages = []
+        no_escape_cell =[]
+        line = []
+        board = []
+        line_with_border = []
         for row in range(BOARD_HEIGHT):
             for cell in range(BOARD_WIDTH):
                 if row == 0 or row == BOARD_HEIGHT-1:
@@ -32,14 +33,14 @@ def create_board(BOARD_WIDTH, BOARD_HEIGHT):
                     random.shuffle(line)
                     line_with_border = ['w']+line+['w']
             board.append(line_with_border)
-
+            
         for row in range(1,(len(board)-1)):
             for cell in range(1,BOARD_WIDTH-1):
-                if board[row][cell] == 'p' and board[-row][cell] == 'p':
+                if board[row][cell] == 'p' and board[row-1][cell] == 'p':
                     passages.append(1)
-                if board[row][cell] == 'p' and board[row][-cell] == 'w' and board[row][+cell] == 'w'\
-                    and board[-row][cell] == 'w' and board[+row][cell] == 'w':
-                    continue
+                if board[row][cell] == 'p' and board[row][cell-1] == 'w' and board[row][cell+1] == 'w'\
+                    and board[row-1][cell] == 'w' and board[row+1][cell] == 'w':
+                    no_escape_cell.append(1)
     return board
 
     # board = [list(30*'w'),\
